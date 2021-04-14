7c54b7d7e9090277c9379ca3bd8e4fe32800e5f2
RA3-SIOUD <sioudlmo@gmail.com>
2021-04-14 12:48:35 +0100

    add login form é

--- src/app/shared/user/login/login.component.ts
+++ src/app/shared/user/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup = this.formBuilder.group({
+    email: ['',Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])],
+    password: ['', Validators.required]
+  });;
+  public returnUrl: string= "";
+  public hide:boolean= false;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private snackBar:MatSnackBar,
+    private authenticationService: AuthenticationService
+  ) { 
+     // redirect to home if already logged in
+     if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/home']);
+     }
+  }
+
+  ngOnInit() {
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/home';
+  }
+
+  onSubmit() {
+    this.authenticationService.login(this.loginForm.value.email, this.loginForm.value.password)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+          this.snackBar.open("Logged in Successufully",'Close')
+        },
+        error => {
+          this.snackBar.open("Failed to login",'Close')
+        });
+  }
+
+  getErrorMessage(){
+    return 'invalid email, try again'
   }
 
 }
